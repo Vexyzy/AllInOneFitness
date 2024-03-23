b95707f4482d3be735cfa5a0161c092557f98d6d
van <vexyzy@yandex.ru>
2024-03-23 09:41:31 +0400

    Add navigationBar in main and add fragment stepsFragment

--- app/src/main/java/com/example/all_in_one_fitness/MainActivity.kt
+++ app/src/main/java/com/example/all_in_one_fitness/MainActivity.kt
@@ -10,9 +10,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.all_in_one_fitness.fragment.StepsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(){
 
     private var sensorManager: SensorManager? = null
 
@@ -25,48 +28,65 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadData()
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    }
-
-    override fun onResume() {
-        super.onResume()
-        running = true
-        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if(stepSensor == null){
-            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
-        } else{
-            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
-        }
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(running){
-            val stepsCounter: TextView = findViewById(R.id.stepsCounter)
-            val progressBar: CircularProgressBar = findViewById(R.id.progress_circular)
-            totalSteps = event!!.values[0]
-            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-            stepsCounter.text = ("$currentSteps")
-
-            progressBar.apply{
-                setProgressWithAnimation(currentSteps.toFloat())
+//        loadData()
+//        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottomNavigation)
+        bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_steps -> {
+                    replaceFragment(StepsFragment())
+                }
             }
+            true
         }
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    private fun replaceFragment(fragment: Fragment)
+    {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.framgeContainer, fragment)
+            .commit()
+    }
 
-    private fun saveData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        editor.putFloat("key1", previousTotalSteps)
-        editor.apply()
-    }
-    private fun loadData() {
-        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
-        val savedNumber = sharedPreferences.getFloat("key1", 0f);
-        Log.d("MainActivity", "$savedNumber")
-        previousTotalSteps = savedNumber
-    }
+//    override fun onResume() {
+//        super.onResume()
+//        running = true
+//        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+//
+//        if(stepSensor == null){
+//            Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+//        } else{
+//            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+//        }
+//    }
+
+//    override fun onSensorChanged(event: SensorEvent?) {
+//        if(running){
+//            val stepsCounter: TextView = findViewById(R.id.stepsCounter)
+//            val progressBar: CircularProgressBar = findViewById(R.id.progress_circular)
+//            totalSteps = event!!.values[0]
+//            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+//            stepsCounter.text = ("$currentSteps")
+//
+//            progressBar.apply{
+//                setProgressWithAnimation(currentSteps.toFloat())
+//            }
+//        }
+//    }
+//
+//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+//
+//    private fun saveData() {
+//        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        editor.putFloat("key1", previousTotalSteps)
+//        editor.apply()
+//    }
+//    private fun loadData() {
+//        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+//        val savedNumber = sharedPreferences.getFloat("key1", 0f);
+//        Log.d("MainActivity", "$savedNumber")
+//        previousTotalSteps = savedNumber
+//    }
 }
---
+++ app/src/main/java/com/example/all_in_one_fitness/fragment/StepsFragment.kt
@@ -0,0 +1,82 @@
+package com.example.all_in_one_fitness.fragment
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import com.example.all_in_one_fitness.MainActivity
+import com.example.all_in_one_fitness.R
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
+
+
+class StepsFragment : Fragment(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_steps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadData()
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if(stepSensor == null){
+            Toast.makeText(requireContext(), "No sensor detected on this device", Toast.LENGTH_SHORT).show()
+        } else{
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(running){
+            val stepsCounter: TextView = requireView().findViewById(R.id.stepsCounter)
+            val progressBar: CircularProgressBar = requireView().findViewById(R.id.progress_circular)
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            stepsCounter.text = ("$currentSteps")
+
+            progressBar.apply{
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
+        }
+    }
+
+    private fun loadData() {
+        val sharedPreferences = requireActivity().getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f);
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
